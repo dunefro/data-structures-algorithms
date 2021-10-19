135fc253449f2920bff712e28dfa8b73302c2024
Vedant Pareek <dunefro@gmail.com>
2021-10-19 09:31:11 +0530

    Added webforms Welcome page

---
+++ golang/section_1/chapter_2/webforms/webforms.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+func Home(writer http.ResponseWriter, reader *http.Request) {
+	var template_html *template.Template
+	template_html = template.Must(template.ParseFiles("main.html"))
+	template_html.Execute(writer, nil)
+}
+
+func main() {
+	log.Println("Server started on port 8000")
+	http.HandleFunc("/", Home)
+	http.ListenAndServe(":8000", nil)
+}
