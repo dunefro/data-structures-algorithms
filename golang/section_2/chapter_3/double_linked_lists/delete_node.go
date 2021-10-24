6ef160e7fd1372af2982fe6076733bb112bca735
Vedant Pareek <dunefro@gmail.com>
2021-10-24 13:15:48 +0530

    Adding delete node

--- golang/section_2/chapter_3/double_linked_lists/delete_node.go
+++ golang/section_2/chapter_3/double_linked_lists/delete_node.go
@@ -9,7 +9,6 @@ func deleteNodeFromBeginning(start *node) (*node, error) {
 	}
 	return start, fmt.Errorf("EmptyList: Not able to delete the node from the list")
 }
-
 func deleteNodeFromEnd(start *node) (*node, error) {
 	if start == nil {
 		return start, fmt.Errorf("EmptyList: Not able to delete the node from the list")
@@ -27,4 +26,28 @@ func deleteNodeFromEnd(start *node) (*node, error) {
 			ptr = ptr.next
 		}
 	}
+}
+func deleteNodeFromGivenValue(start *node, nodeval int) (*node, error) {
+	ptr := start
+	for {
+		if ptr == nil {
+			break
+		}
+		if ptr.val == nodeval {
+			if ptr.prev == nil {
+				start = ptr.next
+			} else {
+				ptr.prev.next = ptr.next
+			}
+			if ptr.next != nil {
+				ptr.next.prev = ptr.prev
+			}
+			return start, nil
+		}
+		ptr = ptr.next
+	}
+	return start, fmt.Errorf("ValueNotFound: Not able to find the value %d in the list", nodeval)
+}
+func deleteList(start *node) *node {
+	return nil
 }
